ea600e695d661032aa51b95f028e1c3f5259f697
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-07-02 12:52:16 +0530

    Feat: Changed herosection design

--- src/components/LandingPage/HeroSection.jsx
+++ src/components/LandingPage/HeroSection.jsx
@@ -1,27 +1,26 @@
+import { IoArrowForwardOutline } from "react-icons/io5";
+import backgroundImg from '../../assets/background.webp'
+
 const HeroSection = () => {
     return (
-        <>
-            <section className="mt-24 mx-auto max-w-screen-xl pb-4 px-4 sm:px-8">
-                <div className="text-center space-y-4">
-                    <h1 className="text-gray-800 font-bold text-4xl md:text-5xl">
-                        Optimize your website for
-                        <span className="text-indigo-600"> Search engine</span>
-                    </h1>
-                    <p className="text-gray-500 max-w-xl mx-auto leading-relaxed">
-                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum
-                    </p>
-                </div>
-                <div className="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
-                    <a href="javascript:void(0)" className="px-10 py-3.5 w-full bg-indigo-600 text-white text-center rounded-md shadow-md block sm:w-auto">
-                        Get started
-                    </a>
-                    <a href="javascript:void(0)" className="px-10 py-3.5 w-full text-gray-500 text-center border rounded-md duration-300 hover:text-indigo-600 hover:shadow block sm:w-auto">
-                        Try it out
-                    </a>
-                </div>
-            </section>
-        </>
+        <section className="mt-24 mb-24 mx-auto max-w-screen-xl pb-4 px-4 sm:px-8">
+            <div className="space-y-4 text-left">
+                <h1 className="text-text font-bold text-4xl md:text-5xl">
+                    MovieSphere <br />
+                    Search TV Shows, Movies Online.
+                </h1>
+                <p className="text-text max-w-xl leading-relaxed">
+                    Step into a world where entertainment knows no boundaries, where your screens come alive with an endless array of captivating stories.
+                </p>
+            </div>
+            <div className="mt-12 flex items-center justify-start">
+                <a href="/" className="w-30 gap-2 flex items-center py-2 px-6 text-center text-text text-base font-bold bg-primary hover:bg-indigo-500 active:bg-indigo-700 active:shadow-none rounded-lg shadow">
+                    <span>Watch Now</span>
+                    <IoArrowForwardOutline className="text-text w-6 h-6" />
+                </a>
+            </div>
+        </section>
     )
 }
 
-export default HeroSection
+export default HeroSection;
