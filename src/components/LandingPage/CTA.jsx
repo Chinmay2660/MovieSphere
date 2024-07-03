22ecac05c2115213077e1ab97e186c1bbeb6f201
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-07-03 12:07:35 +0530

    Feat: Redesigned CTA section

--- src/components/LandingPage/CTA.jsx
+++ src/components/LandingPage/CTA.jsx
@@ -1,21 +1,18 @@
 const CTA = () => {
     return (
-        <section className="py-14">
+        <section className="py-14 pt-8 bg-background">
             <div className="max-w-screen-xl mx-auto px-4 md:text-center md:px-8">
                 <div className="max-w-xl md:mx-auto">
-                    <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
-                        Build the future with us
+                    <h3 className="text-text text-3xl font-semibold sm:text-4xl">
+                        Get an account today
                     </h3>
-                    <p className="mt-3 text-gray-600">
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident.
+                    <p className="mt-5 text-text">
+                        Access free content on all of your devices, sync your list, and continue watching anywhere.
                     </p>
                 </div>
-                <div className="flex gap-3 items-center mt-4 md:justify-center">
-                    <a href="javascript:void(0)" className="inline-block py-2 px-4 text-white font-medium bg-gray-800 duration-150 hover:bg-gray-700 active:bg-gray-900 rounded-lg shadow-md hover:shadow-none">
-                        Get started
-                    </a>
-                    <a href="javascript:void(0)" className="inline-block py-2 px-4 text-gray-800 font-medium duration-150 border hover:bg-gray-50 active:bg-gray-100 rounded-lg">
-                        Learn more
+                <div className="flex gap-3 items-center mt-6 md:justify-center">
+                    <a href="/" className="flex items-center gap-2 py-3 px-6 text-center text-black text-base font-bold bg-text hover:bg-secondary active:shadow-none rounded-lg shadow">
+                        Register Free
                     </a>
                 </div>
             </div>
