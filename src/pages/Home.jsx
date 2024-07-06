0f7522570c3bf6e0cbfeeaf324bc84caeeeee66b
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-07-07 01:09:46 +0530

    Fix: Removed horizontal scroller

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import Banner from '../components/Home/Banner';
-import Card from '../components/Home/Card';
+import CardCarousel from '../components/Home/CardCarousel';
 
 const Home = () => {
   const trendingData = useSelector((state) => state.movieData.bannerData);
@@ -8,14 +8,7 @@ const Home = () => {
   return (
     <div className="relative w-full h-screen">
       <Banner />
-      <div className="container mx-auto px-3 my-10">
-        <h2 className="text-xl font-bold lg:text-2xl mb-2">Trending</h2>
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(230px,1fr))] gap-4">
-          {trendingData.map((data, index) => (
-            <Card key={data.id} data={data} index={index + 1} trending={true}/>
-          ))}
-        </div>
-      </div>
+      <CardCarousel data={trendingData} heading={"Trending Now"}/>
     </div>
   );
 };
