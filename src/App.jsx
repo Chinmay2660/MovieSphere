cb3072d6174c41ef57b7536498bb5a1c252fcbb3
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-08-04 17:31:23 +0530

    Fix: Add promise

--- src/App.jsx
+++ src/App.jsx
@@ -8,63 +8,37 @@ import { useDispatch } from "react-redux"
 import { setBannerData, setImageURL, setNowPlayingData, setTopRatedData, setUpcomingData } from "./reduxStore/Reducer/movieSlice"
 
 const App = () => {
-  const dispatch = useDispatch()
-  const fetchTrendingData = async () => {
-    try {
-      const response = await axiosInstance.get('/trending/all/week')
-      dispatch(setBannerData(response.data.results))
-      console.log(response.data.results)
-    } catch (error) {
-      console.log("error", error)
-    }
-  }
-
-  const fetchUpcomingData = async () => {
-    try {
-      const response = await axiosInstance.get('/movie/upcoming')
-      dispatch(setUpcomingData(response.data.results))
-      console.log(response.data.results)
-    } catch (error) {
-      console.log("error", error)
-    }
-  }
-
-  const fetchTopRatedData = async () => {
-    try {
-      const response = await axiosInstance.get('/movie/top_rated')
-      dispatch(setTopRatedData(response.data.results))
-      console.log(response.data.results)
-    } catch (error) {
-      console.log("error", error)
-    }
-  }
-
-  const fetchNowPlayingData = async () => {
-    try {
-      const response = await axiosInstance.get('/movie/now_playing')
-      dispatch(setNowPlayingData(response.data.results))
-      console.log(response.data.results)
-    } catch (error) {
-      console.log("error", error)
-    }
-  }
-
-  const fetchConfigurationData = async () => {
-    try {
-      const response = await axiosInstance.get('/configuration')
-      dispatch(setImageURL(response.data.images.secure_base_url + "original"))
-    } catch (error) {
-      console.log("error", error)
-    }
-  }
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetchTrendingData()
-    fetchUpcomingData()
-    fetchTopRatedData()
-    fetchNowPlayingData()
-    fetchConfigurationData()
-  }, [])
+    const fetchData = async () => {
+      try {
+        const [
+          trendingResponse,
+          upcomingResponse,
+          topRatedResponse,
+          nowPlayingResponse,
+          configResponse
+        ] = await Promise.all([
+          axiosInstance.get('/trending/all/week'),
+          axiosInstance.get('/movie/upcoming'),
+          axiosInstance.get('/movie/top_rated'),
+          axiosInstance.get('/movie/now_playing'),
+          axiosInstance.get('/configuration'),
+        ]);
+
+        dispatch(setBannerData(trendingResponse.data.results));
+        dispatch(setUpcomingData(upcomingResponse.data.results));
+        dispatch(setTopRatedData(topRatedResponse.data.results));
+        dispatch(setNowPlayingData(nowPlayingResponse.data.results));
+        dispatch(setImageURL(configResponse.data.images.secure_base_url + "original"));
+      } catch (error) {
+        console.error("Failed to fetch data", error);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
 
   return (
     <main className="pb-14 lg:pb-0">
@@ -72,7 +46,6 @@ const App = () => {
       <div>
         <Outlet />
       </div>
-      {/* <Footer /> */}
       <MobileNavigation />
     </main>
   )
