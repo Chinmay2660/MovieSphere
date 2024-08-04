c35062a7a8194d46282598b1525a979670754450
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-08-04 17:39:44 +0530

    Feat: Add APIs to Home instead of App.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,49 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import axiosInstance from '../lib/axiosConfig';
+import {
+  setBannerData,
+  setImageURL,
+  setNowPlayingData,
+  setTopRatedData,
+  setUpcomingData
+} from '../reduxStore/Reducer/movieSlice';
 import Banner from '../components/Home/Banner';
 import CardCarousel from '../components/Home/CardCarousel';
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [
+          trendingResponse,
+          upcomingResponse,
+          topRatedResponse,
+          nowPlayingResponse,
+          configResponse
+        ] = await Promise.all([
+          axiosInstance.get('/trending/all/week'),
+          axiosInstance.get('/movie/upcoming'),
+          axiosInstance.get('/movie/top_rated'),
+          axiosInstance.get('/movie/now_playing'),
+          axiosInstance.get('/configuration'),
+        ]);
+
+        dispatch(setBannerData(trendingResponse.data.results));
+        dispatch(setUpcomingData(upcomingResponse.data.results));
+        dispatch(setTopRatedData(topRatedResponse.data.results));
+        dispatch(setNowPlayingData(nowPlayingResponse.data.results));
+        dispatch(setImageURL(configResponse.data.images.secure_base_url + "original"));
+      } catch (error) {
+        console.error("Failed to fetch data", error);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
+
   const trendingData = useSelector((state) => state.movieData.bannerData);
   const upcomingData = useSelector((state) => state.movieData.upcomingData);
   const topRatedData = useSelector((state) => state.movieData.topRatedData);
