d2fe415064a4b384b3242b5c1c2eb22dc470a1b4
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-08-11 16:48:13 +0530

    Fix: Details page design

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -18,8 +18,8 @@ const DetailsPage = () => {
   const [recommendtionsData, setRecommendationsData] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [playVideo, setPlayVideo] = useState(false)
-  const dispatch = useDispatch()
+  const [playVideo, setPlayVideo] = useState(false);
+  const dispatch = useDispatch();
 
   const fetchData = async () => {
     setLoading(true);
@@ -45,16 +45,16 @@ const DetailsPage = () => {
 
   const fetchConfigurationData = async () => {
     try {
-      const response = await axiosInstance.get('/configuration')
-      dispatch(setImageURL(response?.data?.images?.secure_base_url + "original"))
+      const response = await axiosInstance.get('/configuration');
+      dispatch(setImageURL(response?.data?.images?.secure_base_url + "original"));
     } catch (error) {
-      console.log("error", error)
+      console.log("error", error);
     }
-  }
+  };
 
   useEffect(() => {
     if (imageURL !== undefined) {
-      fetchConfigurationData()
+      fetchConfigurationData();
     }
     fetchData();
   }, [params]);
@@ -67,15 +67,13 @@ const DetailsPage = () => {
     return <div>{error}</div>;
   }
 
-  const duration = (Number(data?.runtime) / 60).toFixed(1).split(".")
-
-  const directorName = castData?.crew?.filter((item) => item?.job === "Director").map((item) => item?.name).join(", ")
-
-  const writerName = castData?.crew?.filter((item) => item?.job === "Writer").map((item) => item?.name).join(", ")
+  const duration = (Number(data?.runtime) / 60).toFixed(1).split(".");
+  const directorName = castData?.crew?.filter((item) => item?.job === "Director").map((item) => item?.name).join(", ");
+  const writerName = castData?.crew?.filter((item) => item?.job === "Writer").map((item) => item?.name).join(", ");
 
   return (
     <div>
-      <div className='w-full h-[300px] relative hidden lg:block'>
+      <div className="w-full h-[200px] sm:h-[300px] relative hidden lg:block">
         <div className="w-full h-full">
           <img
             src={imageURL + data?.backdrop_path}
@@ -86,85 +84,82 @@ const DetailsPage = () => {
         </div>
         <div className="absolute w-full h-full top-0 bg-gradient-to-t from-neutral-900 to-transparent opacity-100"></div>
       </div>
-
-      <div className="container mx-auto px-3 py-16 lg:py-0 flex flex-col lg:flex-row gap-5 lg:gap-10">
-        <div className=" relative mx-auto lg:-mt-40 lg:mx-0 w-fit">
+      <div className="container mx-auto px-3 py-8 lg:py-16 flex flex-col lg:flex-row gap-5 lg:gap-10">
+        <div className="relative mx-auto lg:-mt-40 lg:mx-0 w-fit">
           <img
             src={imageURL + data?.poster_path}
-            alt={`Banner`}
-            className="h-80 w-60 object-cover rounded min-w-60"
+            alt={`Poster`}
+            className="h-60 w-40 lg:h-80 lg:w-60 object-cover rounded"
             loading="lazy"
           />
         </div>
 
-        <div >
-          <h2 className="text-2xl lg:text-3xl font-bold text-white">{data.title ?? data.original_title ?? data.name}</h2>
-          <p className="text-neutral-400 mt-1 ">{data.tagline}</p>
+        <div className="flex-1">
+          <h2 className="text-xl lg:text-3xl font-bold text-white">{data.title ?? data.original_title ?? data.name}</h2>
+          <p className="text-neutral-400 mt-1 text-sm lg:text-base">{data.tagline}</p>
 
           <Divider />
 
-          <div className="flex items-center gap-3">
-            {data?.vote_average && <p className="text-tertiary mt-1">Rating: {Number(data?.vote_average).toFixed(1)}+</p>}
-            {data?.vote_count && <span>|</span>}
-            {data?.vote_count && <p className="text-tertiary mt-1">Views: {Number(data?.vote_count)}+</p>}
-            {data?.runtime && <span>|</span>}
-            {data?.runtime && <p className="text-tertiary mt-1">Duration: {duration[0]}h {duration[1]}m</p>}
+          <div className="flex flex-col lg:flex-row items-start lg:items-center gap-3">
+            {data?.vote_average && <p className="text-tertiary mt-1 text-sm lg:text-base">Rating: {Number(data?.vote_average).toFixed(1)}+</p>}
+            {data?.vote_count && <span className="hidden lg:inline">|</span>}
+            {data?.vote_count && <p className="text-tertiary mt-1 text-sm lg:text-base">Views: {Number(data?.vote_count)}+</p>}
+            {data?.runtime && <span className="hidden lg:inline">|</span>}
+            {data?.runtime && <p className="text-tertiary mt-1 text-sm lg:text-base">Duration: {duration[0]}h {duration[1]}m</p>}
           </div>
 
           <Divider />
 
           <button
             onClick={() => setPlayVideo(true)}
-            className="flex items-center gap-2 py-3 px-6 text-center text-black text-base font-bold bg-text hover:bg-secondary active:shadow-none rounded-lg shadow"
+            className="flex items-center gap-2 py-2 lg:py-3 px-4 lg:px-6 text-center text-black text-sm lg:text-base font-bold bg-text hover:bg-secondary active:shadow-none rounded-lg shadow"
           >
-            <IoPlay className="w-6 h-6  transition-colors duration-300" />
+            <IoPlay className="w-5 h-5 lg:w-6 lg:h-6 transition-colors duration-300" />
             <span>Play Now</span>
           </button>
 
           <Divider />
 
           <div>
-            <h3 className="text-white font-bold text-xl">Overview</h3>
-            <p >{data?.overview}</p>
+            <h3 className="text-white font-bold text-lg lg:text-xl">Overview</h3>
+            <p className="text-neutral-300 text-sm lg:text-base">{data?.overview}</p>
 
             <Divider />
 
-            <div className="flex items-center my-2 gap-3">
-              <p className="text-white mt-1">Status: {data?.status}</p>
-              <span>|</span>
-              <p className="text-white mt-1">
-                Release Date : {moment(data?.release_date ? data?.release_date : data?.first_air_date).format("MMMM Do YYYY")}
+            <div className="flex flex-col lg:flex-row items-start lg:items-center my-2 gap-3">
+              <p className="text-white text-sm lg:text-base">Status: {data?.status}</p>
+              <span className="hidden lg:inline">|</span>
+              <p className="text-white text-sm lg:text-base">
+                Release Date: {moment(data?.release_date ? data?.release_date : data?.first_air_date).format("MMMM Do YYYY")}
               </p>
             </div>
 
             <Divider />
 
             <div>
-              {directorName && <p><span className=" text-white">Direction</span> : {directorName}</p>}
+              {directorName && <p><span className="text-white">Direction</span>: {directorName}</p>}
               {directorName && <Divider />}
-              {writerName && <p><span className=" text-white">Writer</span> : {writerName}</p>}
+              {writerName && <p><span className="text-white">Writer</span>: {writerName}</p>}
               {writerName && <Divider />}
             </div>
 
             <Divider />
 
-            <h2 className="text-lg mb-3 text-white">Cast :</h2>
-            <div className="grid grid-cols-[repeat(auto-fit,96px)] gap-5">
+            <h2 className="text-lg lg:text-xl text-white">Cast:</h2>
+            <div className="grid grid-cols-[repeat(auto-fit,80px)] lg:grid-cols-[repeat(auto-fit,96px)] gap-5">
               {castData?.cast?.filter((item) => item?.profile_path).map((item, index) => (
                 <div key={index} className="flex flex-col items-center justify-start">
-                  <div>
-                    <img
-                      src={imageURL + item?.profile_path}
-                      alt={`Banner`}
-                      className="h-24 w-24 object-cover rounded-full"
-                      loading="lazy"
-                    />
-                  </div>
+                  <img
+                    src={imageURL + item?.profile_path}
+                    alt={`Cast`}
+                    className="h-20 w-20 lg:h-24 lg:w-24 object-cover rounded-full"
+                    loading="lazy"
+                  />
                   <div className="text-center mt-2 leading-tight">
-                    <p className="text-neutral-400 font-bold text-sm max-w-full break-words">
+                    <p className="text-neutral-400 font-bold text-xs lg:text-sm max-w-full break-words">
                       {item?.name.split(" ")[0]}
                     </p>
-                    <p className="text-neutral-400 font-bold text-sm max-w-full break-words">
+                    <p className="text-neutral-400 font-bold text-xs lg:text-sm max-w-full break-words">
                       {item?.name.split(" ")[1]}
                     </p>
                   </div>
