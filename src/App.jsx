970538d5490211e72c99c7f9fd388662a42756e0
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-08-12 11:16:25 +0530

    Fix: Footer design issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
-// import Footer from "./components/Reusables/Footer"
+import Footer from "./components/Reusables/Footer";
 import Header from "./components/Reusables/Header";
 
 const App = () => {
   return (
-    <main className="pb-14 lg:pb-0">
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <div>
+      <div className="flex-grow">
         <Outlet />
       </div>
-    </main>
-  )
-}
+      <Footer />
+    </div>
+  );
+};
 
 export default App
