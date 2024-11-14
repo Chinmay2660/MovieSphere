f399f7848e55588439b44e0f067e75cb002b1067
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-11-15 00:59:11 +0530

    Fix: changed base api

--- src/lib/axiosConfig.js
+++ src/lib/axiosConfig.js
@@ -2,10 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     // baseURL: "http://localhost:8000/api",
-    baseURL: "https://api-moviesphere.vercel.app/api",
-    headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`, 
-    },
+    baseURL: "https://api-moviesphere2660.vercel.app/api",
   });  
 
 export default axiosInstance;
