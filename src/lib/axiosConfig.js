02cb76f9cd5b5042c67eff368f05af0d4b653024
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-11-15 00:40:45 +0530

    Fix: add changes

--- src/lib/axiosConfig.js
+++ src/lib/axiosConfig.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     // baseURL: "http://localhost:8000/api",
     baseURL: "https://api-moviesphere.vercel.app/api",
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`, 
+    },
   });  
 
 export default axiosInstance;
