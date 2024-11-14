c507911bc4ae4adcf47df9cd5a73004bb977475a
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-11-15 00:28:37 +0530

    Fix: add cors config

--- src/lib/axiosConfig.js
+++ src/lib/axiosConfig.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
-    headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
-    },
-});
+    baseURL: "http://localhost:8000/api",
+  });  
 
 export default axiosInstance;
