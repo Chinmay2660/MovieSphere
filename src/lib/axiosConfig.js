4276797ed9fc8b0d492c7173ff47d7dbfbb53b07
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-11-15 00:32:20 +0530

    Fix: add comment code to run locally

--- src/lib/axiosConfig.js
+++ src/lib/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    // baseURL: "http://localhost:8000/api",
+    baseURL: "https://api-moviesphere.vercel.app/api",
   });  
 
 export default axiosInstance;
