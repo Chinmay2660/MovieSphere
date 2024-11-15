cecd02219243872dc3f18855325f494454ff2862
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-11-15 21:26:11 +0530

    Fix: add changes

--- src/lib/axiosConfig.js
+++ src/lib/axiosConfig.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+const runLocally = () => window.location.hostname && /^localhost$/.test(window.location.hostname);
+
+const HOST_URL = "https://api-moviesphere2660.vercel.app/api/";
+
 const axiosInstance = axios.create({
-    // baseURL: "http://localhost:8000/api",
-    baseURL: "https://api-moviesphere2660.vercel.app/api",
+    baseURL: runLocally() ? HOST_URL : ''
   });  
 
 export default axiosInstance;
